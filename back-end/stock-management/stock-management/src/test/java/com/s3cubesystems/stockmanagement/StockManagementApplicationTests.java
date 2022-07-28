1ce5234302aacc7e5eef62236dac417d1bef0032
s3codesystems <s3codesystems@gmail.com>
2022-07-28 23:31:45 +0530

    adding stock and user managemnet service

---
+++ back-end/stock-management/stock-management/src/test/java/com/s3cubesystems/stockmanagement/StockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3cubesystems.stockmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/user-management/user-management/src/test/java/com/s3cubesystems/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3cubesystems.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
