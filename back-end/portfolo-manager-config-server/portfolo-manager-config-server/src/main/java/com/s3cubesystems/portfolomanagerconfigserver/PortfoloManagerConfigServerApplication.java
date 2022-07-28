9a24a4cc2697057f5e274580c63ba173f55bc033
s3codesystems <s3codesystems@gmail.com>
2022-07-28 19:22:32 +0530

    first commit

---
+++ back-end/portfolo-manager-config-server/portfolo-manager-config-server/src/main/java/com/s3cubesystems/portfolomanagerconfigserver/PortfoloManagerConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.s3cubesystems.portfolomanagerconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoloManagerConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoloManagerConfigServerApplication.class, args);
+	}
+
+}
---
+++ back-end/portfolo-manager-config-server/portfolo-manager-config-server/src/test/java/com/s3cubesystems/portfolomanagerconfigserver/PortfoloManagerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3cubesystems.portfolomanagerconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoloManagerConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
